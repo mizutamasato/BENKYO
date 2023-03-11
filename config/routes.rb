8a6c724bf607be3b6278dc5b5696dfe096803a8a
mizutamasato <mizuta1masato@gmail.com>
2023-03-11 11:31:24 +0000

    K

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
+  post 'lists' => 'lists#create' 
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
